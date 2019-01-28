f0ea3d1b61b196579c1bce6cc8f98aec578a88c2
OlgaIsupova <ihoho89@gmail.com>
2019-01-28 15:57:29 +0000

    add gdal installation example and processing

---
+++ gdal_installation.md
@@ -0,0 +1,27 @@
+  218  ~/VirtualEnv/VirtualEnv3/bin/pip3 install gdal
+  219  ~/VirtualEnv/VirtualEnv3/bin/pip install gdal
+  220  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL
+  221  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL==2.3.3
+  222  ~/VirtualEnv/VirtualEnv3/bin/pip install libgdal
+  223  sudo easy_install GDAL
+  224  easy_install GDAL
+  225  sudo apt-get install GDAL
+  226  sudo apt-get build-dep python-gdal
+  227  sudo apt-get install python-dev
+  228  sudo apt-get install python-gdal
+  229  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL
+  230  ls /usr/include/gd
+  231  find . -name "GDAL" -print
+  232  sudo apt-get install libgdal-dev
+  233  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL --install-option="--include-dirs=/usr/include/gdal/"
+  234  ~/VirtualEnv/VirtualEnv3/bin/pip install gdal --global-option=build_ext --global-option="-I/usr/include/gdal/"
+  235  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal"
+  236  ~/VirtualEnv/VirtualEnv3/bin/pip install pygdal
+  237  ~/VirtualEnv/VirtualEnv3/bin/pip install pygdal==2.3.3
+  238  ~/VirtualEnv/VirtualEnv3/bin/pip install pygdal==2.3.2.4
+  239  gdal-config --version
+  240  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL==2.2.3
+  241  sudo apt-get install pytho3-dev
+  242  sudo apt-get install python3-dev
+  243  ~/VirtualEnv/VirtualEnv3/bin/pip install GDAL==2.2.3
+  244  ~/VirtualEnv/VirtualEnv3/bin/pip install gdal==2.2.3 --global-option=build_ext --global-option="-I/usr/include/gdal/"
--- src/DataPreprocessor/preprocess_data_22012019.py
+++ src/DataPreprocessor/preprocess_data_22012019.py
@@ -1,4 +1,5 @@
 import itertools
+from enum import Enum
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +7,9 @@ import cv2
 import logging
 import os
 import pathlib
+import gdal
+from osgeo import gdal_array
+import struct
 
 from PIL import Image
 
@@ -15,6 +19,22 @@ from tqdm import trange, tqdm
 
 class OutOfBoundsException(Exception):
     pass
+
+
+class GdalFileException(Exception):
+    pass
+
+
+class Backend(Enum):
+    PILLOW = 1
+    OPENCV = 2
+    GDAL = 3
+
+class FeatureValue(Enum):
+    UNDEFINED = 0
+    FAULT = 1
+    FAULT_LOOKALIKE = 2
+    NONFAULT = 3
 
 
 class DataPreprocessor22012019:
@@ -60,31 +80,153 @@ class DataPreprocessor22012019:
         pathlib.Path(self.dirs['test']).mkdir(parents=True, exist_ok=True)
         pathlib.Path(self.dirs['all_patches']).mkdir(parents=True, exist_ok=True)
 
-    def load(self):
-        logging.info('loading...')
-        self.elevation = np.array(Image.open(self.data_dir+'tibet_elev.tif'))# elevation map (from the Shuttle Radar Topography Mission), values in meters above sea level
-        self.slope = np.array(Image.open(self.data_dir+'tibet_slope.tif' )) # slope map derived from the elevation, values in degrees from horizontal, 0-90
-        optical_r = cv2.imread(self.data_dir+'tibet_R.tif') # standard red / green / blue optical bands from the Landsat-8 platform, each 0 - 255
-        optical_g = cv2.imread(self.data_dir+'tibet_G.tif')
-        optical_b = cv2.imread(self.data_dir+'tibet_B.tif')
-        self.optical_rgb = np.dstack((optical_r[:,:,0], optical_g[:,:,0], optical_b[:,:,0]))
+    def load_elevation(self, backend):
+        """elevation map (from the Shuttle Radar Topography Mission), values in meters above sea level"""
+        path = self.data_dir + 'tibet_elev.tif'
+        if backend == Backend.PILLOW:
+            self.elevation = np.array(Image.open(path))
+        elif backend == Backend.OPENCV:
+            self.elevation = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        elif backend == Backend.GDAL:
+            self.elevation = np.array(gdal.Open(path, gdal.GA_ReadOnly).ReadAsArray())
+
+    def load_slope(self, backend):
+        """slope map derived from the elevation, values in degrees from horizontal, 0-90"""
+        path = self.data_dir + 'tibet_slope.tif'
+        if backend == Backend.PILLOW:
+            self.slope = np.array(Image.open(path))
+        elif backend == Backend.OPENCV:
+            #todo check why it produces None image
+            self.slope = cv2.imread(path, cv2.IMREAD_LOAD_GDAL)
+            raise NotImplementedError("currently not supported")
+        elif backend == Backend.GDAL:
+            self.slope = np.array(gdal.Open(path, gdal.GA_ReadOnly).ReadAsArray())
+
+    def load_optical(self, backend):
+        """standard red / green / blue optical bands from the Landsat-8 platform, each 0 - 255"""
+        path_r, path_g, path_b = self.data_dir + 'tibet_R.tif', self.data_dir + 'tibet_G.tif', self.data_dir + 'tibet_B.tif'
+        optical_r, optical_g, optical_b = None, None, None
+        if backend == Backend.PILLOW:
+            # todo check this
+            raise NotImplementedError("currently not supported")
+        elif backend == Backend.OPENCV:
+            optical_r = cv2.imread(path_r)[:, :, 0]
+            optical_g = cv2.imread(path_g)[:, :, 0]
+            optical_b = cv2.imread(path_b)[:, :, 0]
+        elif backend == Backend.GDAL:
+            opt_string = '-ot Byte -of GTiff -scale 0 65535 0 255'
+            # todo check how to remove tmp file
+            dataset_r = gdal.Translate(self.data_dir + 'tmp.tif', gdal.Open(path_r, gdal.GA_ReadOnly),
+                                       options=opt_string)
+            optical_r = np.array(dataset_r.ReadAsArray())
+            dataset_g = gdal.Translate(self.data_dir + 'tmp.tif', gdal.Open(path_g, gdal.GA_ReadOnly),
+                                       options=opt_string)
+            optical_g = np.array(dataset_g.ReadAsArray())
+            dataset_b = gdal.Translate(self.data_dir + 'tmp.tif', gdal.Open(path_b, gdal.GA_ReadOnly),
+                                       options=opt_string)
+            optical_b = np.array(dataset_b.ReadAsArray())
+
+        self.optical_rgb = np.dstack((optical_r, optical_g, optical_b))
         logging.warning("optical images are not match in 1-2 pixels in size")
         self.optical_rgb = self.optical_rgb[:self.elevation.shape[0], :self.elevation.shape[1]]
+
+    def load_ir(self, backend):
+        #todo add support for other backends
+        self.nir = cv2.imread(self.data_dir + 'tibet_NIR.tif')  # near infrared from Landsat
+        self.ir = cv2.imread(self.data_dir + 'tibet_IR.tif')  # infrared from Landsat
+        self.swir1 = cv2.imread(self.data_dir + 'tibet_SWIR1.tif')  # shortwave infrared1 from Landsat
+        self.swir2 = cv2.imread(self.data_dir + 'tibet_SWIR2.tif')  # shortwave infrared2 from Landsat
+        self.panchromatic = cv2.imread(
+        self.data_dir + 'tibet_P.tif')  # panchromatic band from Landsat, essentially just total surface reflectance, like a grayscale image of the ground
+
+    def load_features(self, backend):
+        """0 - neutral, undefined content (could include faults--fair area for testing)
+        1 - faults
+        2 - fault lookalikes - features that we think share visual or topographic similarity with faults, but expert interpretation can exclude
+        3 - not-faults - areas that definitely do not include faults, nor things that we think even look like faults, can be used directly for training what faults are not."""
+        path = self.data_dir+'feature_categories.tif'
+        if backend == Backend.PILLOW:
+            self.features = np.array(Image.open(path))
+        elif backend == Backend.OPENCV:
+            self.features = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        elif backend == Backend.GDAL:
+            self.features = np.array(gdal.Open(path, gdal.GA_ReadOnly).ReadAsArray())
+
+    def load(self, backend=Backend.GDAL):
+        logging.info('loading...')
+        self.load_elevation(backend)
+        self.load_slope(backend)
+        self.load_optical(backend)
         plt.imsave(self.data_dir+'data.tif', self.optical_rgb)
 
-        self.nir = cv2.imread(self.data_dir+'tibet_NIR.tif') # near infrared from Landsat
-        self.ir = cv2.imread(self.data_dir+'tibet_IR.tif') # infrared from Landsat
-        self.swir1 = cv2.imread(self.data_dir+'tibet_SWIR1.tif') # shortwave infrared1 from Landsat
-        self.swir2 = cv2.imread(self.data_dir+'tibet_SWIR2.tif') # shortwave infrared2 from Landsat
-        self.panchromatic = cv2.imread(self.data_dir+'tibet_P.tif') # panchromatic band from Landsat, essentially just total surface reflectance, like a grayscale image of the ground
-        features_map = Image.open(self.data_dir+'feature_categories.tif')
-        self.features = np.array(features_map) #0 - neutral, undefined content (could include faults--fair area for testing)
-        # 1 - faults
-        # 2 - fault lookalikes - features that we think share visual or topographic similarity with faults, but expert interpretation can exclude
-        # 3 - not-faults - areas that definitely do not include faults, nor things that we think even look like faults, can be used directly for training what faults are not.
-        # features[features > 0] = 1
-        # plt.imsave('features.png', features*255)
+        self.load_ir(backend)
+        self.load_features(backend)
+
         logging.info('loaded')
+
+    def parse_meta_with_gdal(self):
+        # based on https://www.gdal.org/gdal_tutorial.html
+        #Opening the File
+        dataset = gdal.Open(self.data_dir+'tibet_R.tif', gdal.GA_ReadOnly)
+        if not dataset:
+            raise GdalFileException()
+
+        scale = '-scale 0 65535 0 255'
+        options_list = [
+            '-ot Byte',
+            '-of GTiff',
+            scale
+        ]
+        options_string = " ".join(options_list)
+
+        dataset = gdal.Translate(self.data_dir+'tmp.tif', dataset, options=options_string)
+
+        #arr = dataset.ReadAsArray()
+
+        # Getting Dataset Information
+        print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
+                                     dataset.GetDriver().LongName))
+        print("Size is {} x {} x {}".format(dataset.RasterXSize,
+                                            dataset.RasterYSize,
+                                            dataset.RasterCount))
+        print("Projection is {}".format(dataset.GetProjection()))
+        geotransform = dataset.GetGeoTransform()
+        if geotransform:
+            print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
+            print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
+
+        # Fetching a Raster Band
+        band = dataset.GetRasterBand(1)
+        print("Band Type={}".format(gdal.GetDataTypeName(band.DataType)))
+
+        min = band.GetMinimum()
+        max = band.GetMaximum()
+        if not min or not max:
+            (min, max) = band.ComputeRasterMinMax(True)
+        print("Min={:.3f}, Max={:.3f}".format(min, max))
+
+        if band.GetOverviewCount() > 0:
+            print("Band has {} overviews".format(band.GetOverviewCount()))
+
+        if band.GetRasterColorTable():
+            print("Band has a color table with {} entries".format(band.GetRasterColorTable().GetCount()))
+
+        #dataset = gdal.Translate(self.data_dir+'tmp.tif', dataset, options=gdal.TranslateOptions(outputType=gdal.GDT_Byte, scaleParams=[0, 65535, 0, 255]))
+        #dataset = gdal.Translate(self.data_dir+'tmp.tif', gdal.TranslateOptions(["-of", "GTiff", "-ot", "Byte", "-scale", "0 65535 0 255"]))
+
+
+
+        # Reading Raster Data
+        scanline = band.ReadRaster(xoff=0, yoff=0,
+                                   xsize=band.XSize, ysize=1,
+                                   buf_xsize=band.XSize, buf_ysize=1,
+#                                   buf_type=gdal.GDT_Float32)
+                                   buf_type=gdal.GDT_Byte)
+
+        #tuple_of_floats = struct.unpack('f' * band.XSize, scanline)
+        tuple_of_floats = struct.unpack('b' * band.XSize, scanline)
+
+        dataset = None
 
     def borders_from_center(self, center):
         left_border = center[0] - self.patch_size[0] // 2
@@ -239,11 +381,11 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.WARN)
     loader = DataPreprocessor22012019("../../data/Data22012019/")
     #loader.get_elevation_with_features_mask()
-    loader.prepare_folders()
+    #loader.prepare_folders()
     #loader.prepare_train()
     #loader.prepare_valid()
     #loader.prepare_test()
-    loader.prepare_test_with_labels()
+    #loader.prepare_test_with_labels()
     #loader.prepare_all_patches()
-
+    loader.parse_meta_with_gdal()
 
