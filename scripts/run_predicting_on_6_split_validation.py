71c05eb3d5655f626469f8fb202b174eda297942
ihoho <ihoho89@gmail.com>
2019-03-26 21:23:47 +0000

    prepare experiment on train on 6

---
+++ scripts/run_predicting_on_6_split_validation.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.extend(['../../EarthScienceFaultDetection'])
+
+from src.DataPreprocessor.data_preprocessor import Mode
+from src.LearningKeras.net_architecture import cnn_150x150x5
+from src.LearningKeras.train import KerasTrainer
+from src.pipeline import global_params
+from src.postprocessing.postprocessor import PostProcessor
+
+trainer = KerasTrainer(model_generator=None, ensemble_size=1)
+
+model = cnn_150x150x5()
+model.load_weights('training_on_6_split_validation/model.h5')
+trainer.models.append(model)
+
+datasets = list(range(8))
+
+for (preprocessor_ind, data_preprocessor_generator) in enumerate(global_params.data_preprocessor_generators):
+    if preprocessor_ind not in datasets:
+        continue
+
+    data_preprocessor = data_preprocessor_generator(Mode.TEST)
+    boxes, probs = trainer.apply_for_sliding_window(
+        data_preprocessor=data_preprocessor, patch_size=(150, 150), stride=25, batch_size=16,
+        channels=[0, 1, 2, 3, 4])
+    original_2dimage_shape = (data_preprocessor.get_data_shape()[0], data_preprocessor.get_data_shape()[1])
+    faults_postprocessor = PostProcessor(boxes=boxes, probs=probs[:, 0],
+                                         original_2dimage_shape=original_2dimage_shape)
+    res_faults = faults_postprocessor.heatmaps(mode="mean")
+    data_preprocessor.data_io_backend.write_surface("training_on_6_split_validation/heatmaps_faults_{}.tif".format(preprocessor_ind), res_faults)
+
+    nonfaults_postprocessor = PostProcessor(boxes=boxes, probs=probs[:, 1],
+                                                original_2dimage_shape=original_2dimage_shape)
+    res_nonfaults = nonfaults_postprocessor.heatmaps(mode="mean")
+    data_preprocessor.data_io_backend.write_surface("training_on_6_split_validation/heatmaps_nonfaults_{}.tif".format(preprocessor_ind),
+                                                        res_nonfaults)
---
+++ scripts/run_training_on_6_split_validation.py
@@ -0,0 +1,51 @@
+import pathlib
+
+import h5py
+import numpy as np
+import tensorflow as tf
+
+import sys
+sys.path.extend(['../../EarthScienceFaultDetection'])
+
+from src.LearningKeras.net_architecture import cnn_150x150x3, cnn_150x150x5
+
+with h5py.File('../train_data/regions_6/data.h5', 'r') as hf:
+    imgs = hf['imgs'][:]
+    lbls = hf['lbls'][:]
+
+permuted_ind = np.random.permutation(imgs.shape[0])
+imgs = imgs[permuted_ind]
+lbls = lbls[permuted_ind]
+
+imgs = imgs[:, :, :, 0:5]
+
+train_ratio = 0.80
+
+train_len = int(imgs.shape[0] * train_ratio)
+imgs_train = imgs[:train_len].copy()
+lbls_train = lbls[:train_len].copy()
+imgs_valid = imgs[train_len:].copy()
+lbls_valid = lbls[train_len:].copy()
+imgs = None
+lbls = None
+
+
+model = cnn_150x150x5()
+
+callbacks = [
+    tf.keras.callbacks.CSVLogger(filename='training_on_6_split_validation/log.csv', separator=',', append=False)
+]
+
+output_path = "training_on_6_split_validation"
+pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+
+history = model.fit(x=imgs_train,
+                    y=lbls_train,
+                    epochs=10,
+                    verbose=1,
+                    callbacks=callbacks,
+                    validation_data=(imgs_valid, lbls_valid),
+                    workers=0,
+                    use_multiprocessing=False)
+
+model.save_weights(output_path + '/model.h5')
