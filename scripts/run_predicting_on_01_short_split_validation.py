2d70fde4738d6291baa679c4ab0d5f7c50b67af6
OlgaIsupova <ihoho89@gmail.com>
2019-05-21 16:45:34 +0100

    separate full prediction on trained net and postprocesssing heatmpas steps

--- scripts/run_predicting_on_01_short_split_validation.py
+++ scripts/run_predicting_on_01_short_split_validation.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.extend(['../../EarthScienceFaultDetection'])
 
-from src.pipeline.predicting import predict
+from src.pipeline.predicting import predict, postprocess
 
 predict(datasets=list(range(6)), models_folder='training_on_01_short_split_validation', classes=2,
-        channels=[0, 1, 2, 3, 4], heatmap_mode="mean", stride=25, batch_size=16)
+        channels=[0, 1, 2, 3, 4], stride=25, batch_size=16)
+postprocess(datasets=list(range(6)), models_folder='training_on_01_short_split_validation', heatmap_mode="mean")
--- src/pipeline/predicting.py
+++ src/pipeline/predicting.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import h5py
 import numpy as np
 import tensorflow as tf
 
@@ -14,7 +15,7 @@ np.random.seed(1)
 tf.set_random_seed(2)
 
 
-def predict(datasets: List[int], models_folder, classes, channels, heatmap_mode="max", stride=25, batch_size=20):
+def predict(datasets: List[int], models_folder, classes, channels, stride=25, batch_size=20):
     if classes == 3:
         model = cnn_150x150x5_3class()
     elif classes == 2:
@@ -47,12 +48,30 @@ def predict(datasets: List[int], models_folder, classes, channels, heatmap_mode=
         boxes, probs = trainer.apply_for_sliding_window(
             data_preprocessor=data_preprocessor, patch_size=(150, 150), stride=stride, batch_size=batch_size,
             channels=channels)
+
+        with h5py.File(models_folder + f'{models_folder}/sliding_window_{preprocessor_ind}.h5', 'w') as hf:
+            hf.create_dataset("boxes", data=boxes)
+            hf.create_dataset("probs", data=probs)
+
+
+def postprocess(datasets: List[int], models_folder, heatmap_mode="max"):
+
+    for (preprocessor_ind, data_preprocessor_generator) in enumerate(global_params.data_preprocessor_generators):
+        if preprocessor_ind not in datasets:
+            continue
+
+        data_preprocessor = data_preprocessor_generator(Mode.TEST)
+
+        with h5py.File(models_folder + f'{models_folder}/sliding_window_{preprocessor_ind}.h5', 'w') as hf:
+            boxes = hf["boxes"][:]
+            probs = hf["probs"][:]
+
         original_2dimage_shape = (data_preprocessor.get_data_shape()[0], data_preprocessor.get_data_shape()[1])
         faults_postprocessor = PostProcessor(boxes=boxes, probs=probs[:, 0],
                                              original_2dimage_shape=original_2dimage_shape)
         res_faults = faults_postprocessor.heatmaps(mode=heatmap_mode)
         data_preprocessor.data_io_backend.write_image(
-            "{}/heatmaps_trained_on_01_long_faults_{}_probs".format(models_folder, preprocessor_ind),
+            f"{models_folder}/heatmaps_probs_{preprocessor_ind}",
             res_faults * 100)
         data_preprocessor.data_io_backend.write_surface(
-            "{}/heatmaps_faults_{}".format(models_folder, preprocessor_ind), res_faults)
+            f"{models_folder}/heatmaps_faults_{res_faults}")
