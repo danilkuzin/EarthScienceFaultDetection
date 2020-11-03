6368fb7045adf791d5f084ef5237a31d7dda8e6c
kuzind <danilkuzin>
2020-11-03 00:28:05 +0000

    add torchvision transforms from new version of torchvision

---
+++ scripts/ttest_segmentation_unet_training_upd_augmentations.py
@@ -0,0 +1,85 @@
+import sys
+import numpy as np
+import torch
+
+sys.path.extend(['../../EarthScienceFaultDetection'])
+#os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+from src.pipeline_torch.dataset_torchvision import ToTensor, RandomRotation, \
+    RandomHorizontalFlip, RandomVerticalFlip, ColorJitter
+
+from src.LearningTorch.net_architecture import UNet, LossBinary, FocalLoss, \
+    LossCrossDice, LossMulti, LossMultiSemiSupervised, Res34_Unet, \
+    LossMultiSemiSupervisedEachClass
+from src.pipeline_torch.training import datasets_on_single_files_torch, \
+    train_on_preloaded_single_files_torch_unet, \
+    datasets_on_single_files_torch_segmentation
+
+from src.config import data_path
+
+import torchvision
+
+
+np.random.seed(1000)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+cnn_model = Res34_Unet(n_input_channels=7, n_classes=3)
+criterion = LossMultiSemiSupervisedEachClass(
+    device=device, nll_weight=1, jaccard_weight=5,
+    focal_weight=12, ignore_classes_for_nll=[3],
+    ignore_classes_for_jaccard=[0],
+    alpha=0.9, gamma=2, reduction='mean')
+
+#LossMultiSemiSupervised(jaccard_weight=5, ignore_class_for_nll=3, ignore_classes_for_jaccard=[0])
+# LossMulti(jaccard_weight=5, num_classes=3) # nn.CrossEntropyLoss()
+# LossCrossDice(jaccard_weight=5)
+# FocalLoss(reduction='mean')
+# FocalLoss(gamma=0.5, alpha=0.5)
+# LossBinary(jaccard_weight=5) # nn.BCEWithLogitsLoss()
+
+optimizer = torch.optim.Adam(cnn_model.parameters(), lr=1e-4)
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10,
+                                       gamma=0.1)
+
+# im = np.random.randint(255, size=(1, 5, 156, 156)).astype(np.float32)
+# im_tensor = torch.tensor(im)
+#
+# output = cnn_model(im_tensor)
+# print(output.shape)
+
+
+batch_size = 4
+num_workers = 0
+
+cnn_model = cnn_model.to(device)
+
+transform_train = torch.nn.Sequential(
+    [
+        ToTensor(),
+        RandomRotation(degrees=45),
+        RandomHorizontalFlip(),
+        RandomVerticalFlip(),
+        ColorJitter(),
+    ]
+)
+
+train_dataset, train_dataset_size, valid_dataset, valid_dataset_size = \
+    datasets_on_single_files_torch_segmentation(
+        device=device,
+        regions=[6], path_prefix=f'{data_path}/train_data',
+        channels=[0, 1, 2, 3, 4, 5, 6],
+        train_ratio=0.8, batch_size=batch_size,
+        num_workers=num_workers,
+        transform=transform_train
+)
+
+train_on_preloaded_single_files_torch_unet(
+    cnn_model, train_dataset, train_dataset_size, valid_dataset, valid_dataset_size,
+    folder=f"{data_path}/results/semisupervised_one_class",
+    epochs=100,
+    batch_size=batch_size,
+    optimizer=optimizer,
+    criterion=criterion,
+    scheduler=exp_lr_scheduler)
+
---
+++ src/pipeline_torch/dataset_torchvision.py
@@ -0,0 +1,103 @@
+import torch
+import torch.utils.data as data
+import torchvision
+
+import random
+
+import numpy as np
+
+"""
+This is a wrapper for torchvision.transforms for multi-target transformations. 
+See also torchvision segmentation transforms as examples
+"""
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        return {
+            'image': torch.transforms.functional.to_tensor(image),
+            'label': torch.as_tensor(np.array(label), dtype=torch.int64)
+        }
+
+# todo add resize, randomcrop
+
+class RandomHorizontalFlip(torch.nn.Module):
+    """Randomly flip input horizontally"""
+
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+
+    def forward(self, sample):
+        image, label = sample['image'], sample['label']
+
+        if torch.rand(1) < self.p:
+            image = torchvision.transforms.functional.hflip(image)
+            label = torchvision.transforms.functional.hflip(label)
+
+        return {'image': image, 'label': label}
+
+
+class RandomVerticalFlip(torch.nn.Module):
+    """Randomly flip input vertically"""
+
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+
+    def forward(self, sample):
+        image, label = sample['image'], sample['label']
+
+        if torch.rand(1) < self.p:
+            image = torchvision.transforms.functional.vflip(image)
+            label = torchvision.transforms.functional.vflip(label)
+
+        return {'image': image, 'label': label}
+
+
+
+
+# todo RandomPerspective
+# todo RandomResizedCrop
+# todo RandomSizedCrop
+
+class ColorJitter(torchvision.transforms.ColorJitter):
+    def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
+        super().__init__()
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+    def forward(self, sample):
+        return {
+            'image': super(ColorJitter, self).forward(sample['image']),
+            'label': sample['label']
+        }
+
+
+class RandomRotation(torch.nn.Module):
+    """Rotate on random number of 90 degree rotation"""
+
+    def __init__(self, degrees):
+        super().__init__()
+        self.degrees = torchvision.transforms._setup_angle(degrees, name="degrees", req_sizes=(2,))
+
+    def forward(self, sample):
+        image, label = sample['image'], sample['label']
+
+        angle = torchvision.transforms.RandomRotation.get_params(degrees=self.degrees)
+        image = torchvision.transforms.functional.rotate(img=image, angle=angle)
+        label = torchvision.transforms.functional.rotate(img=label, angle=angle)
+
+        return {'image': image, 'label': label}
+
+
+# todo RandomAffine
