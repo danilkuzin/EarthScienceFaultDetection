f268f1084e7eb74bfdcfb6e54d9fb36206657951
danilkuzin <danilkuzin@gmail.com>
2021-06-03 14:54:45 +0100

    prediction on 7

--- scripts/write_saved_image_to_gdal_two_fault_classes.py
+++ scripts/write_saved_image_to_gdal_two_fault_classes.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from src.DataPreprocessor.DataIOBackend.gdal_backend import GdalBackend
 from src.config import data_path
 
-folder = f"{data_path}/results/unet_on_6_rgb_elev_slope_nir_hazmap_one_class_dice_focal_alpha_0.9_semisupervised"
+folder = f"{data_path}/results/unet_on_6_rgb_elev_nir_tri_flow_incision_two_fault_classes_3xlookalike_dice_focal_alpha_0.9_semisupervised"
 prediction_region = 7
 saved_image_path = folder + f'/prediction_on_{prediction_region}.npz'
 prediction = numpy.load(saved_image_path)['prediction']
@@ -22,35 +22,66 @@ gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
                         eval(gdal_params['geotransform']))
 
 gdal_backend.write_float(geotiff_path, prediction_front_range)
+
+
+mask_prediction_front_range = prediction[1]
+mask_prediction_front_range_max = mask_prediction_front_range.max()
+mask_prediction_front_range_min = mask_prediction_front_range.min()
+mask_prediction_front_range_scaled = (mask_prediction_front_range-mask_prediction_front_range_min)/(mask_prediction_front_range_max-mask_prediction_front_range_min)
+clip_lower = 0.4 # 0.3 - hazmap on 6, 0.4 - hazmap on 7
+clip_higher = 1 # 0.7 - hazmap on 6, 0.75 - hazmap on 7
+mask_prediction_front_range_clipped = (mask_prediction_front_range_scaled.clip(clip_lower, clip_higher)-clip_lower)/(clip_higher-clip_lower)
+seaborn.distplot(mask_prediction_front_range_clipped.flatten())
+plt.title("front range")
+plt.show()
 #
-#
-# mask_prediction_front_range = prediction[1]
-# mask_prediction_front_range_max = mask_prediction_front_range.max()
-# mask_prediction_front_range_min = mask_prediction_front_range.min()
-# mask_prediction_front_range_scaled = (mask_prediction_front_range-mask_prediction_front_range_min)/(mask_prediction_front_range_max-mask_prediction_front_range_min)
-# clip_lower = 0.2 # 0.4# 0.3 - hazmap on 6, 0.4 - hazmap on 7
-# clip_higher = 0.75 # 0.75 # 0.7 - hazmap on 6, 0.75 - hazmap on 7
-# mask_prediction_front_range_clipped = (mask_prediction_front_range_scaled.clip(clip_lower, clip_higher)-clip_lower)/(clip_higher-clip_lower)
-# seaborn.distplot(mask_prediction_front_range_clipped.flatten())
-# plt.title("front range")
+# mask_prediction_basin = prediction[2]
+# mask_prediction_basin_max = mask_prediction_basin.max()
+# mask_prediction_basin_min = mask_prediction_basin.min()
+# mask_prediction_basin_scaled = (mask_prediction_basin-mask_prediction_basin_min)/(mask_prediction_basin_max-mask_prediction_basin_min)
+# clip_lower = 0.4
+# clip_higher = 1 #0.9
+# mask_prediction_basin_clipped = (mask_prediction_basin_scaled.clip(clip_lower, clip_higher)-clip_lower)/(clip_higher-clip_lower)
+# seaborn.distplot(mask_prediction_basin_clipped.flatten())
+# plt.title("basin")
 # plt.show()
-# #
-# # mask_prediction_basin = prediction[2]
-# # mask_prediction_basin_max = mask_prediction_basin.max()
-# # mask_prediction_basin_min = mask_prediction_basin.min()
-# # mask_prediction_basin_scaled = (mask_prediction_basin-mask_prediction_basin_min)/(mask_prediction_basin_max-mask_prediction_basin_min)
-# # clip_lower = 0.4
-# # clip_higher = 1 #0.9
-# # mask_prediction_basin_clipped = (mask_prediction_basin_scaled.clip(clip_lower, clip_higher)-clip_lower)/(clip_higher-clip_lower)
-# # seaborn.distplot(mask_prediction_basin_clipped.flatten())
-# # plt.title("basin")
-# # plt.show()
-# #
-# class_prediction = numpy.argmax(prediction, axis=0)
-# mask_front_range_binary = ((class_prediction == 1) * 255).astype(numpy.uint8)
-# mask_basin_binary = ((class_prediction == 2) * 255).astype(numpy.uint8)
 #
-# geotiff_path = folder + f'/prediction_front_range_on_{prediction_region}_geo.tiff'
+class_prediction = numpy.argmax(prediction, axis=0)
+mask_front_range_binary = ((class_prediction == 1) * 255).astype(numpy.uint8)
+mask_basin_binary = ((class_prediction == 2) * 255).astype(numpy.uint8)
+
+
+geotiff_path = folder + f'/prediction_front_range_on_{prediction_region}_geo_greyscale.tiff'
+
+gdal_backend = GdalBackend()
+with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
+    gdal_params = yaml.safe_load(stream)
+gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
+                        eval(gdal_params['geotransform']))
+
+gdal_backend.write_surface(geotiff_path, mask_prediction_front_range_scaled,
+                           colour=False)
+
+plt.imshow(mask_prediction_front_range_scaled, cmap='gray')
+plt.title('strike slip greyscale')
+plt.show()
+
+
+geotiff_path = folder + f'/prediction_front_range_on_{prediction_region}_geo.tiff'
+
+gdal_backend = GdalBackend()
+with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
+    gdal_params = yaml.safe_load(stream)
+gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
+                        eval(gdal_params['geotransform']))
+
+gdal_backend.write_surface(geotiff_path, mask_prediction_front_range_clipped)
+
+plt.imshow(mask_prediction_front_range_clipped)
+plt.title('front range')
+plt.show()
+#
+# geotiff_path = folder + f'/prediction_basin_on_{prediction_region}_geo.tiff'
 #
 # gdal_backend = GdalBackend()
 # with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
@@ -58,37 +89,23 @@ gdal_backend.write_float(geotiff_path, prediction_front_range)
 # gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
 #                         eval(gdal_params['geotransform']))
 #
-# gdal_backend.write_surface(geotiff_path, mask_prediction_front_range_clipped)
+# gdal_backend.write_surface(geotiff_path, mask_prediction_basin_clipped)
 #
-# plt.imshow(mask_prediction_front_range_clipped)
-# plt.title('front range')
+# plt.imshow(mask_prediction_basin_clipped)
+# plt.title('basin')
 # plt.show()
-# #
-# # geotiff_path = folder + f'/prediction_basin_on_{prediction_region}_geo.tiff'
-# #
-# # gdal_backend = GdalBackend()
-# # with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
-# #     gdal_params = yaml.safe_load(stream)
-# # gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
-# #                         eval(gdal_params['geotransform']))
-# #
-# # gdal_backend.write_surface(geotiff_path, mask_prediction_basin_clipped)
-# #
-# # plt.imshow(mask_prediction_basin_clipped)
-# # plt.title('basin')
-# # plt.show()
-# #
-# geotiff_path = folder + f'/prediction_front_range_on_{prediction_region}_geo_binary.tiff'
 #
-# gdal_backend = GdalBackend()
-# with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
-#     gdal_params = yaml.safe_load(stream)
-# gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
-#                         eval(gdal_params['geotransform']))
-#
-# gdal_backend.write_surface(geotiff_path, mask_front_range_binary)
-#
-# plt.imshow(mask_front_range_binary)
-# plt.show()
+geotiff_path = folder + f'/prediction_front_range_on_{prediction_region}_geo_binary.tiff'
+
+gdal_backend = GdalBackend()
+with open(f"{data_path}/preprocessed/{prediction_region}/gdal_params.yaml", 'r') as stream:
+    gdal_params = yaml.safe_load(stream)
+gdal_backend.set_params(gdal_params['driver_name'], gdal_params['projection'],
+                        eval(gdal_params['geotransform']))
+
+gdal_backend.write_surface(geotiff_path, mask_front_range_binary)
+
+plt.imshow(mask_front_range_binary)
+plt.show()
 
 
